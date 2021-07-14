88d3c7ee537f770e816402c2e6e91dc821a6ee09
Kevin Van <kevin.van@student.unimelb.edu.au>
2021-07-14 16:26:59 +1000

    some UI shuffling

--- app/src/main/java/com/example/chaintimer/model/ChainTimer.kt
+++ app/src/main/java/com/example/chaintimer/model/ChainTimer.kt
@@ -1,6 +1,6 @@
 package com.example.chaintimer.model
 
-class ChainTimer(val seconds: Int) {
+class ChainTimer(val seconds: Int, val name: String = "Timer") {
     override fun toString(): String {
         return "$seconds seconds"
     }
